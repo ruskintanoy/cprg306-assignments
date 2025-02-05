464a706e52ba023edf792c512df89d755ac6fabe
Ruskin Tanoy <ruskintanoyy@gmail.com>
2025-02-04 21:32:15 -0700

    Week 2 Assignment
    Initial commit of week-2 assignment.

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,18 +1,10 @@
-
-import Link from 'next/link';
-
-const StudentInfo = () => {
+export default function StudentInfo() {
   return (
-    <div>
+  <div>
       <h1>Ruskin Tanoy</h1>
       <p>
-        GitHub Repository:{' '}
-        <Link href="https://github.com/ruskintanoy/cprg306-assignments" target="_blank" rel="noopener noreferrer">
-          Visit here
-        </Link>
+        <a href="https://github.com/ruskintanoy/cprg306-assignments"> Link of my Github Repository</a>
       </p>
-    </div>
+  </div>
   );
-};
-
-export default StudentInfo;
+}
