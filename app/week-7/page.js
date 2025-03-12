6c8abdbf1ac23fe8413efd92711d36f6895da00e
Ruskin Tanoy <166348536+ruskintanoy@users.noreply.github.com>
2025-03-11 23:19:58 -0600

    Update page.js

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,10 +1,22 @@
+"use client";
+
+import { useState } from "react";
 import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
 
 export default function Page() {
-    return (
-      <main>           
-        <h1 className="font-bold text-4xl m-5">Shopping List</h1>
-        <ItemList />
-    </main>
-    );
+  const [items, setItems] = useState(itemsData);
+
+  const handleAddItem = (newItem) => {      
+      setItems([...items, newItem]);
+  }
+
+  return (
+    <main>           
+      <h1 className="font-bold text-4xl m-5">Shopping List</h1>
+      <NewItem onAddItem={handleAddItem} />
+      <ItemList items={items} />
+  </main>
+  );
 }
